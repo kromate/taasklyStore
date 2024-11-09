f57656c483da90d1b3cc792f69c079d5e737131f
kromate <akpananthony33@gmail.com>
2024-11-09 09:58:57 +0100

    init

--- app_config.ts
+++ app_config.ts
@@ -6,7 +6,7 @@
 export default {
     keepalive: true,
     head: {
-        title: 'Bookings - Simplified Bookings, Amplified Business',
+        title: 'Store - Simplified Store, Amplified Business',
         htmlAttrs: { lang: 'en' },
         viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
         bodyAttrs: { class: 'overflow-x-hidden' },
@@ -19,38 +19,38 @@ export default {
             },
             {
                 name: 'title',
-                content: 'Bookings - Simplified Bookings, Amplified Business'
+                content: 'Store - Simplified Store, Amplified Business'
             },
             {
                 name: 'description',
-                content: 'Create personalized booking pages, manage appointments effortlessly, and get paid seamlessly. Tailored for African entrepreneurs, perfect for everyone.'
+                content: 'Create your online store instantly, manage products effortlessly, and process payments seamlessly. Built for African businesses, perfect for everyone.'
             },
             {
                 name: 'twitter:title',
-                content: 'Bookings - Simplified Bookings, Amplified Business'
+                content: 'Store - Simplified Store, Amplified Business'
             },
-            { name: 'twitter:image', content: 'https://bookings.taaskly.site/og2.png' },
+            { name: 'twitter:image', content: 'https://stores.taaskly.site/og2.png' },
             {
                 name: 'twitter:description',
-                content: 'Create personalized booking pages, manage appointments effortlessly, and get paid seamlessly. Tailored for African entrepreneurs, perfect for everyone.'
+                content: 'Create your online store instantly, manage products effortlessly, and process payments seamlessly. Built for African businesses, perfect for everyone.'
             },
             { name: 'twitter:card', content: 'summary_large_image' },
             { name: 'twitter:site', content: '@kromate_24' },
             { name: 'twitter:creator', content: '@kromate_24' },
             {
                 property: 'og:title',
-                content: 'Bookings | Simplified Bookings, Amplified Business'
+                content: 'Store | Simplified Store, Amplified Business'
             },
             { name: 'google-site-verification', content: 'tWttF6w3RHPlNPm5u7KSRgh4lgkRUZ2Bwl6QzECjY18' },
             { property: 'og:type', content: 'website' },
-            { property: 'og:url', content: 'https://bookings.taaskly.site/' },
-            { property: 'og:image', content: 'https://bookings.taaskly.site/og2.png' },
-            { property: 'og:image:secure_url', content: 'https://bookings.taaskly.site/og2.png' },
+            { property: 'og:url', content: 'https://stores.taaskly.site/' },
+            { property: 'og:image', content: 'https://stores.taaskly.site/og2.png' },
+            { property: 'og:image:secure_url', content: 'https://stores.taaskly.site/og2.png' },
             { property: 'og:image:type', content: 'image/png' },
-            { property: 'og:site_name', content: 'Bookings' },
+            { property: 'og:site_name', content: 'Store' },
             {
                 property: 'og:description',
-                content: 'Create personalized booking pages, manage appointments effortlessly, and get paid seamlessly. Tailored for African entrepreneurs, perfect for everyone.'
+                content: 'Create your online store instantly, manage products effortlessly, and process payments seamlessly. Built for African businesses, perfect for everyone.'
             },
 
             { name: 'format-detection', content: 'telephone=no' }
